c6fc7e5ae17db4ced171c299d776be271f6c49b7
Tim <pfeffer.tim@gmx.net>
2022-09-20 15:26:00 +0200

    add mpu6050, adjust ini

--- src/headtracker.cpp
+++ src/headtracker.cpp
@@ -1,6 +1,15 @@
 #include "headtracker.h"
 #include <Arduino.h>
+#include <Wire.h>
+
+#ifdef USE_QMC5883
 #include <DFRobot_QMC5883.h>
+#endif
+
+#ifdef USE_MPU6050
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#endif
 
 /////////// FUNCTION DEFS ///////////
 
@@ -8,22 +17,50 @@ extern void sendMSPViaEspnow(mspPacket_t *packet);
 
 /////////////////////////////////////
 
-
+#ifdef USE_QMC5883
 DFRobot_QMC5883 compass(&Wire, /*I2C addr*/QMC5883_ADDRESS);
+#endif
+
+#ifdef USE_MPU6050
+MPU6050 mpu;
+
+// MPU control/status vars
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 
 
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+#endif
 
 void
 Headtracker::Init()
 {
     delay(0);
-    Wire.begin(17,16);
+    Wire.begin(PIN_SDA,PIN_SCL);
 
+#ifdef USE_QMC5883
     while (!compass.begin())
     {
-        Serial.println("Could not find a valid compass 5883 sensor, check wiring!");
+        Serial.println("Could not find a valid compass _MC5883 sensor!");
         delay(500);
     }
+#endif
+
+#ifdef USE_MPU6050
+    mpu.initialize();
+    while(mpu.dmpInitialize() != 0)
+    {
+        Serial.println("Could not initialize dmp of MPU6050!");
+        delay(500);
+    }
+    mpu.setDMPEnabled(true);
+    mpuIntStatus = mpu.getIntStatus();
+    packetSize = mpu.dmpGetFIFOPacketSize();
+#endif
 
 }
 
@@ -32,43 +69,94 @@ Headtracker::Loop(uint32_t now)
 {
     if((now-m_last)>500){
 
-
-        float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
-        compass.setDeclinationAngle(declinationAngle);
-        sVector_t mag = compass.readRaw();
-        compass.getHeadingDegrees();
-        // Serial.print("Degress = ");
-        // Serial.println(mag.HeadingDegress);
-
-
         mspPacket_t packet;
         packet.reset();
         packet.makeCommand();
         packet.function = MSP_ELRS_BACKPACK_HEADTRACKER;
-        // static int n=0;
-        int angle = mag.HeadingDegress*10;
-        if(angle>2700) angle=angle-3600;
-        else if(angle>1800) angle=-900;
-        else if(angle>900) angle=900;
-        Serial.print("Angle*10 = ");
-        Serial.println(angle);
+
+        int yaw=0;
+        int pitch=0;
+
+#ifdef USE_QMC5883
+        float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
+        compass.setDeclinationAngle(declinationAngle);
+        sVector_t mag = compass.readRaw();
+        compass.getHeadingDegrees();
+        yaw = mag.HeadingDegress*10;
+#endif
+
+#ifdef USE_MPU6050
+        yaw = ypr[0]*1800/PI;
+        pitch = ypr[1]*1800/PI;
+
+        //accelerometer
+        Serial.print("ypr\t");
+        Serial.print(ypr[0] * 180/PI);
+        Serial.print("\t");
+        Serial.print(ypr[1] * 180/PI);
+        Serial.print("\t");
+        Serial.println(ypr[2] * 180/PI);
+
+#endif
+
+        if(yaw>2700) yaw=yaw-3600;
+        else if(yaw>1800) yaw=-900;
+        else if(yaw>900) yaw=900;
+        
+        if(pitch>2700) pitch=pitch-3600;
+        else if(pitch>1800) pitch=-450;
+        else if(pitch>450) pitch=450;
+        
+
+        
+    
+        
+        Serial.print("yaw*10 = ");
+        Serial.print(yaw);
+        Serial.print(", pitch*10 = ");
+        Serial.println(pitch);
         // mapping from 0-360 to -900 - 900
 
         
-        // packet.addByte((n>>8) %4);  // payload1
-        // packet.addByte(n%256);  // payload2 x=p1*256+p2
-        // packet.addByte((-n>>8) %4);  // payload3
-        // packet.addByte(-n%256);  // payload4 y=p3*256+p4
-        packet.addByte(angle>>8);  // payload1
-        packet.addByte(angle);  // payload2 x=p1*256+p2
-        packet.addByte(0);  // payload3
-        packet.addByte(0);  // payload4 y=p3*256+p4
+        packet.addByte(yaw>>8);  // payload1
+        packet.addByte(yaw);  // payload2 y=p1*256+p2
+        packet.addByte(pitch>>8);  // payload3
+        packet.addByte(pitch);  // payload4 x=p3*256+p4
         sendMSPViaEspnow(&packet);
-
-        // n++;
 
         m_last = now;
     }
+
+#ifdef USE_MPU6050
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    uint16_t fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if ((mpuIntStatus & 0x02) > 0) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+        // accelerometer
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    }
+#endif
+
 }
 
 Headtracker::Headtracker(Stream *port)
